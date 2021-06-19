06b03cbcebdd261e84bef7bbf4ab5b8f8e4b4050
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-19 20:24:11 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,10 @@
-module Findable 
+module Findable
 def find_by_name(name)
    self.all.detect{|a| a.name == name}
- end 
-  
+ end
+
   def find_by_name(name)
     self.all.detect{|a| a.name == name}
   end
-  
-end 
+
+end
